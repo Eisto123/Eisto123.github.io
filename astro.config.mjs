327dfd7f6bc32778bab8b4a65b6b28b4e17e3cac
Eisto123 <lzm505553681@gmail.com>
2025-01-12 16:39:27 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
-const owner = "astrojs";
-// https://astro.build/config
 export default defineConfig({
   site: 'https://Eisto123.github.io',
-  integrations: [tailwind()],
-
 });
