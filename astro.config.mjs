e5dd1a17204943fae37848f880d5e58881cdf151
Eisto123 <lzm505553681@gmail.com>
2025-02-06 22:06:03 +0000

    Add project detail

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
+
 const owner = "astrojs";
 // https://astro.build/config
 export default defineConfig({
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
+
+
+
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
